7dedf2ac43d1b574126b1b363bafd57847c57c09
Vince <github_vjdw@vwingate.sent.com>
2020-04-13 18:44:08 +0100

    Tidy Now Playing text

--- Pages/Shared/Components/NowPlaying/NowPlayingViewComponent.cs
+++ Pages/Shared/Components/NowPlaying/NowPlayingViewComponent.cs
@@ -13,6 +13,15 @@ namespace PiperPicker.Pages.Components.NowPlaying
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var nowPlaying = await MopidyProxy.GetNowPlaying();
+
+            nowPlaying.State = string.IsNullOrWhiteSpace(nowPlaying.State)
+                ? "Idle"
+                : $"{nowPlaying.State.Substring(0, 1).ToUpperInvariant()}{nowPlaying.State.Substring(1)}";
+
+            nowPlaying.Name = string.IsNullOrWhiteSpace(nowPlaying.Name)
+                ? "Empty Play Queue"
+                : nowPlaying.Name;
+
             return View("Default", nowPlaying);
         }
     }
