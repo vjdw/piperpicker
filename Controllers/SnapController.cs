817983bbfa8273d72cd1c2c9ce4b1109b358bd78
Vince <github_vjdw@vwingate.sent.com>
2021-12-14 21:02:58 +0000

    Update packages

--- Controllers/SnapController.cs
+++ Controllers/SnapController.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.SignalR;
-using Newtonsoft;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using PiperPicker.Hubs;
 using PiperPicker.Proxies;
 
