7441d99b41a97fb00776196e6e4452610cee8980
Vince <github@wngt.uk>
2026-03-14 22:31:16 +0000

    Added progress bar

--- Proxies/MopidyProxy.cs
+++ Proxies/MopidyProxy.cs
@@ -1,8 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace PiperPicker.Proxies
 {
